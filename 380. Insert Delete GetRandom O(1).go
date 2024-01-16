ca53c4e23ed66e2a3df69597d74ab7017eb0b979
Cyberguru1 <107911619+Cyberguru1@users.noreply.github.com>
2024-01-16 05:14:30 +0000

    second solution

--- 380. Insert Delete GetRandom O(1).go
+++ 380. Insert Delete GetRandom O(1).go
@@ -59,3 +59,75 @@ func (this *RandomizedSet) GetRandom() int {
  * param_2 := obj.Remove(val);
  * param_3 := obj.GetRandom();
  */
+
+
+
+// second solution
+
+import "math/rand"
+
+type RandomizedSet struct {
+	container map[int]bool          
+}
+
+
+
+func Constructor() RandomizedSet {
+
+	return RandomizedSet{
+		container: map[int]bool{},
+	}
+
+}
+
+
+func (this *RandomizedSet) Insert(val int) bool {
+
+	if _, ok := this.container[val]; ok{
+		return false
+	}
+
+	this.container[val] = true    
+	return true
+
+}
+
+
+func (this *RandomizedSet) Remove(val int) bool {
+
+
+	if _, ok := this.container[val]; ok{
+		delete(this.container, val)
+		return true
+	}
+
+	return false
+
+}
+
+
+func (this *RandomizedSet) GetRandom() int {
+
+	lenn := len(this.container)
+	randVal := rand.Intn(lenn)
+
+	count := 0
+
+	for k, _ := range this.container {
+		if count == randVal {
+			return k
+		}
+		count++
+	}
+
+	return 0
+}
+
+
+/**
+ * Your RandomizedSet object will be instantiated and called as such:
+ * obj := Constructor();
+ * param_1 := obj.Insert(val);
+ * param_2 := obj.Remove(val);
+ * param_3 := obj.GetRandom();
+ */

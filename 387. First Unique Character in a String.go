9aeea55e96c9dac00fc8d41a1e6cfbe0229a4482
Cyberguru1 <107911619+Cyberguru1@users.noreply.github.com>
2024-02-05 22:58:22 +0000

    387. First Unique Character in a String

---
+++ 387. First Unique Character in a String.go
@@ -0,0 +1,18 @@
+// Time C. O(n), Space C. O(n)
+// runtime 3ms
+
+func firstUniqChar(s string) int {
+	var counts = make([]int, 26)
+
+	for i := 0; i < len(s); i++ {
+		counts[s[i] - 'a']++
+	}
+
+	for i := 0; i < len(s); i++ {
+		if counts[s[i] - 'a'] == 1 {
+			return i
+		}
+	}
+
+	return -1
+}

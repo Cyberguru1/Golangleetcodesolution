9a4a414466afb77780753fde140f846fbf3ee305
Hamza Saidu <107911619+Cyberguru1@users.noreply.github.com>
2024-03-05 18:49:34 +0000

    1750. Minimum Length of String After Deleting Similar Ends

---
+++ 1750. Minimum Length of String After Deleting Similar Ends.go
@@ -0,0 +1,20 @@
+// Time C. O(n), Space C. O(1)
+// runtime 
+
+
+func minimumLength(s string) (res int) {
+	l, r := 0, len(s)-1
+
+	for l < r && s[l] == s[r] {
+		char = s[l]
+		for l <= r && s[l] == char {
+			l++
+		}
+		for r >= l && s[r] == char {
+			r--
+		}
+	}
+
+	return r - l + 1
+}
+

e37ab73f479e6bcee767a973b300c78b177e22d7
Hamza Saidu <107911619+Cyberguru1@users.noreply.github.com>
2024-04-20 22:45:55 +0000

    200. Number of Islands

---
+++ 1992. Find All Groups of Farmland.go
@@ -0,0 +1,37 @@
+func findFarmland(land [][]int) [][]int {
+	m, n := len(land), len(land[0])
+	var answer [][]int
+
+	for r := 0; r < m; r++ {
+		for c := 0; c < n; c++ {
+			if land[r][c] == 1 {
+				rMax, cMax := -1, -1
+				dfs(land, r, c, &rMax, &cMax)
+				answer = append(answer, []int{r, c, rMax, cMax})
+			}
+		}
+	}
+
+	return answer
+}
+
+func dfs(land [][]int, r, c int, rMax, cMax *int) {
+	if r < 0 || r >= len(land) || c < 0 || c >= len(land[0]) || land[r][c] == 0 {
+		return
+	}
+
+	// Mark current cell as visited
+	land[r][c] = 0
+
+	// Update rMax and cMax if current cell is more 'bottom-right'
+	if r > *rMax {
+		*rMax = r
+	}
+	if c > *cMax {
+		*cMax = c
+	}
+
+	// Explore the cell to the right (c + 1) and below (r + 1)
+	dfs(land, r, c+1, rMax, cMax) // Move right
+	dfs(land, r+1, c, rMax, cMax) // Move down
+}
---
+++ 200. Number of Islands.go
@@ -0,0 +1,30 @@
+// TIme C. O(M*N)
+
+func numIslands(grid [][]byte) (count int ){
+	
+	var dfs func(grid [][]byte, r, c int)
+
+	dfs = func(grid [][]byte, r, c int){
+		if r < 0 || r >= len(grid) || c < 0 || c >= len(grid[r]) || grid [r][c] != '1'{
+			return
+		}
+
+		grid[r][c] = 'v' // to show we have visited
+		// move on all direction
+		dfs(grid, r + 1, c) // move down
+		dfs(grid, r - 1, c) // move up
+		dfs(grid, r, c + 1) // move right
+		dfs(grid, r, c - 1) // move left
+	}
+
+	for i := 0; i < len(grid); i++ {
+		for j := 0; j < len(grid[i]); j++ {
+			if grid[i][j] == '1' {
+				count++
+				dfs(grid, i, j)
+			}
+		}
+	}
+
+	return
+}

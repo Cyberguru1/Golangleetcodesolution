896a18dfe552e346deaf6e6046624712614304ec
Hamza Saidu <107911619+Cyberguru1@users.noreply.github.com>
2024-04-28 12:10:05 +0000

    first push

---
+++ 2997. Minimum Number of Operations to Make Array XOR Equal to K.go
@@ -0,0 +1,15 @@
+// Time C. O(1), Space C O(1)
+// runtime 10ms
+
+func minOperations(nums []int, k int) int {
+	res := k
+	for _, n := range nums {
+		res ^= n
+	}
+	var ans int
+	for res > 0 {
+		ans += res % 2
+		res = res >> 1
+	}
+	return ans
+}

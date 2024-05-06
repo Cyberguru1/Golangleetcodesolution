7cfdc5cbc76dabca7ccf4019eb4e74eff175596b
Hamza Saidu <107911619+Cyberguru1@users.noreply.github.com>
2024-05-06 12:12:00 +0000

    done git add .

---
+++ 2816. Double a Number Represented as a Linked List.go
@@ -0,0 +1,35 @@
+// Time C. O(n), Space C. O(1)
+// runtime 2ms
+
+
+/**
+ * Definition for singly-linked list.
+ * type ListNode struct {
+ *     Val int
+ *     Next *ListNode
+ * }
+ */
+func doubleIt(head *ListNode) *ListNode {
+
+	if head.Val > 4 {
+		head = &ListNode{0, head}
+	}
+
+	trav := head
+
+	for trav.Next != nil {
+
+		trav.Val = (trav.Val * 2 ) % 10 
+
+		if trav.Next.Val > 4 {
+			trav.Val++
+		}
+
+		trav = trav.Next
+	}
+
+	trav.Val = (trav.Val * 2) % 10
+
+	return head
+}
+

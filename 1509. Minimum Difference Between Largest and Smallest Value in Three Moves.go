0bbeb00bb786b42db610fa6c3874017ce80a672b
Hamza Saidu <107911619+Cyberguru1@users.noreply.github.com>
2024-07-03 12:15:24 +0000

    1509. Minimum Difference Between Largest and Smallest Value in Three Moves

---
+++ 1509. Minimum Difference Between Largest and Smallest Value in Three Moves.go
@@ -0,0 +1,20 @@
+// Time C. O(n), Space C. O(1)
+
+func minDifference(nums []int) (res int) {
+
+	lent := len(nums)
+	if lent <= 4 { return }
+
+	sort.Ints(nums)
+	res = int(^uint(0) >> 1)
+
+	l, r := 3, 0
+
+	for l >= 0 && r <= 3 {
+		res = min(max((nums[l]-nums[lent-1-r]), (nums[l]-nums[lent-1-r])*-1), res)
+		r++
+		l--
+	}
+
+	return
+}
